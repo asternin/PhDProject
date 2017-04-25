e54451464acf949e1b2dc7328cbce1211f93e1a9
Avital Sternin <asternin@ssc-macpro-eeg.ssc.uwo.ca>
2017-04-25 11:21:04 -0400

    no message

--- Music and Memory - Lucy/PilotTest/LyricMod_TestAll.m
+++ Music and Memory - Lucy/PilotTest/LyricMod_TestAll.m
@@ -1,14 +1,10 @@
 commandwindow; 
 test=input('Press 1 to open on this screen, Press 2 to open on secondary screen: '); %press 1 for debugging/testing
 subID=input('Subject ID: ', 's');
-group=input('Group A - press 1, Group B - press 2: ');
+%group=input('Group A - press 1, Group B - press 2: ');
 testStart=datestr(now,'yyyymmdd');
 
-if group==1;
-    load('LyricMod_GroupA.mat');
-elseif group ==2;
-    load('LyricMod_GroupB.mat');
-end
+load('LyricMod_ALL_v2.mat');
 
 black=[0,0,0];
 white=[255,255,255];
@@ -29,30 +25,26 @@ text{1}=['Thank you for your participating in this study. \n\n\n\n '...
 text{2}=['The task has now ended \n\n' ...
     'Thank you for participating! \n\n' ...
     'Press any key to exit.'];   
-OMG OMG ok!
 %% prep stuff
+len=length(lyrics);
 responses=[];
-responses.data=zeros(1,12);
+responses.data=zeros(1,len);
 responses.avg=[];
 responses.button=[];
 rng(sum(100*clock)); %reset value so randperm is different for each participant.
-order=randperm(24); %determine random order of stimuli 
-order=order(1:12); %only ask 12 questions
+order=randperm(len); %determine random order of stimuli 
+order=order(1:len);
 responses.order=order;
 
 %% welcome text
 DrawFormattedText(win, text{1}, 'center', 'center', white); Screen('Flip', win);
 KbWait([], 2);
 %% 
-for i=1:12;
+for i=1:len;
+    
     j=order(i);
     corr=char(lyrics(j,1));
-    rand=randperm(2); %determine which modification will be presented
-    if rand(1)==1;
-        oth=char(lyrics(j,2));
-    elseif rand(1)==2;
-        oth=char(lyrics(j,3));
-    end
+    oth=char(lyrics(j,2));
         
     rand=randperm(2); %determine which order the lyrics will be presented in
     if rand(1)==1;
@@ -88,7 +80,7 @@ for i=1:12;
         end
     end
 end
-responses.avg = (sum(responses.data)/12)*100;
+responses.avg = (sum(responses.data)/len)*100;
 save([subID '_responses_ ' testStart ' .mat'], 'responses');
 WaitSecs(0.5);
 
