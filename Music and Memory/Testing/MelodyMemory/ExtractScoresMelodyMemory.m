54acf92c11c3a35b804dd4a23d2295c9532c7730
asternin <avitalsternin@gmail.com>
2018-03-06 13:05:49 -0500

    no message

---
+++ Music and Memory/Testing/MelodyMemory/ExtractScoresMelodyMemory.m
@@ -0,0 +1,18 @@
+%navigate to folder
+files=dir('*.xlsx');
+results=cell(length(files),13);
+for i=1:length(files);
+    [~,~,raw]=xlsread(files(i).name);
+    results(i,1)={files(i).name};
+    raw(1,:)=[]; %remove header row
+    for j=1:size(raw,1);
+        if cell2mat(raw(j,3)) == 1 && strcmp(raw(j,8),'SAME')==1
+            results(i,j+1)={1}; %CORRECT identification of SAME
+        elseif cell2mat(raw(j,3)) == 0 && strcmp(raw(j,8),'DIFFERENT')==1
+            results(i,j+1)={1}; %CORRECT identification of DIFFERENT
+        else 
+            results(i,j+1)={0}; %INCORRECT identification
+        end
+    end
+end
+save('MelodyMemoryResults.mat','results')
