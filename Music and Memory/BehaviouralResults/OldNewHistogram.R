63aa5803239ef0f17af24fe9b0f8a530c783d6ad
asternin <avitalsternin@gmail.com>
2018-10-26 15:23:51 -0400

    no message

--- Music and Memory/BehaviouralResults/OldNewHistogram.R
+++ Music and Memory/BehaviouralResults/OldNewHistogram.R
@@ -2,6 +2,20 @@ fname <-"file:///C:/Users/asternin/Documents/PhDProject.git/Music and Memory/Beh
 #fname<-"~/Documents/Western/Academics/PhDProject.git/Music and Memory/fMRI analysis/CovData.csv"
 data<-read.table(fname, sep=",",header=FALSE)
 
+
+library(ggplot2)
+ggplot(data)+
+  aes(x=V1)+
+  geom_histogram(breaks=seq(70,100, by=5),color="white",fill="#134663")+
+  labs(x="% Correct",y="Frequency") +
+  theme_classic() +
+  theme(axis.text = element_text(size=18),
+        axis.title = element_text(size=20),
+        legend.position="none")
+ggsave("OldNew_SfN.pdf",device="pdf")
+
+#scale_x_continuous(breaks=c(70,80,90,100),labels=c("70","80","90","100")) + # Ticks from 70-100, every 10
+
 hist(data$V1, 
      main="",
      xlim = c(70,100),
--- Music and Memory/fMRI analysis/LyrModplot.R
+++ Music and Memory/fMRI analysis/LyrModplot.R
@@ -9,5 +9,16 @@ data$V1=factor(data$V1,
                   labels=c("pre-scan1", "lab1", "lab2", "lab3", "lab4", "post-scan2")
                   )
 
+library(ggplot2)
+ggplot(data) +
+  aes(x=V1, y=V2) +
+  geom_violin(lwd=1,color="#EC008c") +
+  geom_jitter(size=3, alpha = 0.7, color="#134663") +
+  labs(x="Experimental Session",y="% correct") +
+  theme_classic() +
+  theme(axis.text = element_text(size=18),
+        axis.title = element_text(size=20))
+ggsave("LyrMemory_SfN.pdf",device="pdf")
+
 plot(data$V1,data$V2, xlab="lab session", ylab="lyric modification score")
 #abline(fit<-lm(data$V2~data$V1))
