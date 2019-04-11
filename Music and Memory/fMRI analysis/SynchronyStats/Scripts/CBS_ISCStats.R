2a9806d650c8dae4f995e415871eb79809922d77
Avital Sternin <>
2019-04-11 14:02:13 -0400

    no message

--- Music and Memory/fMRI analysis/SynchronyStats/Scripts/CBS_ISCStats.R
+++ Music and Memory/fMRI analysis/SynchronyStats/Scripts/CBS_ISCStats.R
@@ -5,7 +5,9 @@ library(ggplot2)
 library(plyr)
 
 #ISCdata<-readr::read_csv("/Users/asternin/Documents/PhDProject/Music and Memory/fMRI analysis/SynchronyStats/Data/old/FP.csv")
-ISCdata<-readr::read_csv("/Users/asternin/Documents/PhDProject/Music and Memory/fMRI analysis/SynchronyStats/Data/old/Auditory.csv")
+ISCdata<-readr::read_csv("/Users/avitalsternin/Documents/Western/Academics/PhDProject.git/Music and Memory/fMRI analysis/SynchronyStats/Data/old/FP.csv")
+#ISCdata<-readr::read_csv("/Users/asternin/Documents/PhDProject/Music and Memory/fMRI analysis/SynchronyStats/Data/old/Auditory.csv")
+
 ISCdata<-ISCdata[-c(6),]
 ISCdata<-rename(ISCdata,c("1_song1"="A1_1", "1_song2"="S1_1", "1_song3"="S2_1", "1_song4"="W1_1",
                     "1_song5"="W2_1", "1_song6"="I1_1", "1_song7"="I2_1", "1_song8"="A2_1",
@@ -13,7 +15,9 @@ ISCdata<-rename(ISCdata,c("1_song1"="A1_1", "1_song2"="S1_1", "1_song3"="S2_1", 
                     "2_song5"="W2_2", "2_song6"="I1_2", "2_song7"="I2_2", "2_song8"="A2_2"))
 
 
-CBSdata<-readr::read_csv("/Users/asternin/Documents/PhDProject/Music and Memory/BehaviouralResults/CBSScoresForISC.csv")
+#CBSdata<-readr::read_csv("/Users/asternin/Documents/PhDProject/Music and Memory/BehaviouralResults/CBSScoresForISC.csv")
+CBSdata<-readr::read_csv("/Users/avitalsternin/Documents/Western/Academics/PhDProject.git/Music and Memory/BehaviouralResults/CBSScoresForISC.csv")
+
 CBSdata<-CBSdata[-c(6),]
 CBSdata<-CBSdata[,-1] #remove ID variable
 CBSdata<-rename(CBSdata,c("Paired Associates"="PA", "Odd One Out (Max)"="OOO", "Polygons"="P", "Grammatical Reasoning"="GR",
@@ -32,14 +36,14 @@ W2<-rowMeans(select(data, "W1_2",'W2_2')) #ses 1 whole data together
 
 ##REGRESSIONS for CBS scores
 #null model = age+MusicExpRating+MusicYrsMax+NumOfLang+Listens
-s<-I1
+s<-S2
 m0song<-lm(s~age+MusicExpRating+MusicYrsMax+NumOfLang+Listens,data=data)
 mallsong<-lm(s~DT+OOO+SP+GR+DS+TS+PA+SS+FM+R+P+ML+age+MusicExpRating+MusicYrsMax+NumOfLang+Listens,data=data)
 mbestsong<-step(m0song,list(lower=m0song,upper=mallsong),direction="both")
 summary(mbestsong)
 
 #null model = age
-s<-W2
+s<-A1
 m0song<-lm(s~age,data=data)
 mallsong<-lm(s~DT+OOO+SP+GR+DS+TS+PA+SS+FM+R+P+ML+age,data=data)
 mbestsong<-step(m0song,list(lower=m0song,upper=mallsong),direction="both")
