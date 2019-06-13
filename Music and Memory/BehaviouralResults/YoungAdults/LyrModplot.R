aaa71844644e6b9f17da1890c81950beb0de4c8a
asternin <avitalsternin@gmail.com>
2019-06-13 15:01:36 -0400

    no message

--- Music and Memory/BehaviouralResults/YoungAdults/LyrModplot.R
+++ Music and Memory/BehaviouralResults/YoungAdults/LyrModplot.R
@@ -23,7 +23,9 @@ ggsave("LyrMemory_SfN.pdf",device="pdf")
 plot(data$V1,data$V2, xlab="lab session", ylab="lyric modification score")
 
 listens<-(c(0,4.54375,2.16895,2.8625,2.125,3.23125))
-plot(listens, type="l")
+xlabels<-(c('pre-scan1','scan1-lab1','lab1-lab2','lab2-lab3','lab3-lab4','lab4-scan2'))
+plot(listens, type="l",xaxt="n",xlab="Sessions")
+axis(1,at=1:6, labels=xlabels)
 #abline(fit<-lm(data$V2~data$V1))
 
 pre <- data[ which(data$V1=='pre-scan1'), ] 
@@ -32,3 +34,6 @@ lab2 <- data[ which(data$V1=='lab2'), ]
 lab3 <- data[ which(data$V1=='lab3'), ] 
 lab4 <- data[ which(data$V1=='lab4'), ] 
 post <- data[ which(data$V1=='post-scan2'), ] 
+
+t.test(pre$V2, post$V2)
+t.test(lab3$V2, post$V2)
